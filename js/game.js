65fbf003ff413ceb6bb2176d749c47229b713c19
Derek Garcia <dgarcia2@hawaii.edu>
2024-12-23 15:27:13 -1000

    feat: complete stubbed out student logic

--- js/game.js
+++ js/game.js
@@ -5,9 +5,20 @@ import {Deck} from "./deck.js";
 'MAX_CARD_LIMIT' is a global variable. Globals are used when we want to use the same value in multiple places in
 our code. This way if we want to change it in the future, we just need to change it in one place!
  */
-export const MAX_CARD_LIMIT = 21;
+const MAX_CARD_LIMIT = 21;
+
+let PLAYER_ONE_SCORE = 0;
 
 export const HIT_BUTTON = document.getElementById('hit-btn');
+export const STAY_BUTTON = document.getElementById('stay-btn');
+STAY_BUTTON.addEventListener("click", function () {
+    if (PLAYER_ONE_SCORE > MAX_CARD_LIMIT) {
+        alert("You lost! You went over by" + PLAYER_ONE_SCORE - MAX_CARD_LIMIT + "!");
+    } else {
+        alert("You win with a score of " + PLAYER_ONE_SCORE + "!");
+    }
+    end_game();
+})
 
 export const CARD_IMAGE = document.getElementById('current_card');
 
@@ -15,13 +26,23 @@ export const CARD_IMAGE = document.getElementById('current_card');
 export function play_game() {
     // Create a new deck to use for this game; "We are opening a new physical deck from a box"
     let deck = new Deck();
-    let player_one_score = 0;
-
+    PLAYER_ONE_SCORE = 0;
+    // Reset the card back
+    CARD_IMAGE.src = "../images/deck/card_back.png";
     // Update the hit button so a new card is drawn every time
     HIT_BUTTON.removeEventListener("click", draw_and_update_card);
     HIT_BUTTON.addEventListener("click", function () {
         // we can use player_one_score since only player one will be clicking the button
-        player_one_score = draw_and_update_card(deck, player_one_score);
-    });
+        PLAYER_ONE_SCORE = draw_and_update_card(deck, PLAYER_ONE_SCORE);
 
+        if (PLAYER_ONE_SCORE > MAX_CARD_LIMIT)
+            end_game();
+
+    });
+}
+
+export function end_game() {
+    HIT_BUTTON.disabled = true;
+    STAY_BUTTON.disabled = true;
+    alert("Refresh the page to play again!")
 }
--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 import {CARD_IMAGE, play_game} from "./js/game.js";
 
-// TODO #1: Add your with your name!
+// === START #1.1 ===
+// TODO #1.1: Add your with your name!
 // Remove the '/*' and '*/ once you do
 /*
-const PLAYER_NAME = ???;
+const PLAYER_NAME ? ???;
 document.getElementById('???').textContent = ???;   // todo: add hint in readme
 console.log("Let's play " + PLAYER_NAME +"!");
 */
+
+// === END #1.1 ===
 
 export function draw_and_update_card(deck, player_score) {
     let card = deck.draw_card();    // Draw card from the deck
@@ -14,30 +17,41 @@ export function draw_and_update_card(deck, player_score) {
     CARD_IMAGE.src = "/images/deck/" + card.get_file_name();    // Update the source image to the drawn card
 
     let value_str = card.value;     // Get the string value of the card
-    // TODO #3.1: Update the score!
+
+    // === START #2.1 ===
+    // TODO #2.1: Complete the if-else tree!
     // Remove the '/*' and '*/ once you do
     /*
-    if(value_str === "ace"){
-        PLAYER_ONE_SCORE += 1;
-    } else if(value_str === "two"){
-        PLAYER_ONE_SCORE += 2;
+    if(value_str == "ace"){
+        player_score = player_score + 1;
+    } else if(value_str == "two"){
+        player_score = player_score + 2;
     } ...
      */
+    // === END #2.1 ===
+
     console.log("Player One's score is " + player_score);
-    // TODO #3.2: Update the score!
+
+    // === START #2.2 ===
+    // TODO #2.2: Update the score!
     // Remove the '/*' and '*/ once you do
     /*
     document.???('???').innerText = ???;    // todo: add hint in readme
      */
+    // === END #2.2 ===
 
-    // TODO #4: Did the player lose?
-    // REMEMBER: The player's score is stored in the 'PLAYER_ONE_SCORE' variable
+    // === START #3 ===
+    // TODO #3: Did the player lose?
+    // REMEMBER: The player's score is stored in the 'player_score' variable
     // Remove the '/*' and '*/ to once you do
     /*
     YOUR CODE HERE
      */
+    // === END #3 ===
 
     return player_score;
 }
 
+
 play_game();
+
